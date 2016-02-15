fe3ac40de0fa729503ba51314a46c7c960a45f7f
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 14:27:19 +0100

    Début étape 3 : base pour TrackerScreen et ses tests

---
+++ src/components/Gauge.js
@@ -0,0 +1,13 @@
+import { amber500, green500, orange500, red500 } from 'material-ui/styles/colors'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import LinearProgress from 'material-ui/LinearProgress'
+
+import { getCompletionRatio } from '../lib/helpers'
+
+const Gauge = () => (
+  <p>COMING SOON: GAUGE</p>
+)
+
+export default Gauge
