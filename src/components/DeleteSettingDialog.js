57521fe908ee430b3924dbd83eb5ad559bc9f3f1
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 15:19:40 +0100

    Début étape 14 : SettingsScreen

---
+++ src/components/DeleteSettingDialog.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import ContentClear from 'material-ui/svg-icons/content/clear'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+
+const DeleteSettingDialog = ({ goal = {}, onCancel, onDelete, open }) => {
+  const actions = [
+    <FlatButton label='Ouh là, non !' secondary onTouchTap={onCancel} keyboardFocused />,
+    <FlatButton label='Adios !' primary onTouchTap={onDelete} icon={<ContentClear />} />
+  ]
+
+  return (
+    <Dialog
+      title='Supprimer un objectif'
+      actions={actions}
+      open={open}
+      onRequestClose={onCancel}
+    >
+      <p>Supprimer l’objectif « {goal.name} » ?</p>
+    </Dialog>
+  )
+}
+
+DeleteSettingDialog.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired
+}
+
+export default DeleteSettingDialog
